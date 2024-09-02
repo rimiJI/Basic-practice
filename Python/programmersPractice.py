c54932ab4e157c3f6e0bac3e7f84eebaeaf86d69
Hyerim JI <rimirimijj@gmail.com>
2024-09-02 16:20:58 +0900

    programmers_#[PCCE 기출문제] 3번 / 나이 계산

--- Python/programmersPractice.py
+++ Python/programmersPractice.py
@@ -1,5 +1,23 @@
 
 
+
+
+
+
+
+#[PCCE 기출문제] 3번 / 나이 계산
+year = int(input())
+age_type = input()
+
+if age_type == "korea":
+    answer = 2030-year+1
+
+elif age_type == "Year":
+    answer=2030-year
+
+print(answer)
+
+
 #[PCCE 기출문제] 4번 / 저축
 start = int(input())
 before = int(input())
