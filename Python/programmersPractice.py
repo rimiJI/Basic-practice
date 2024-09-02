4b97d86fb0148e39a2a829ffead0b41c4e2514ff
Hyerim JI <rimirimijj@gmail.com>
2024-09-02 18:41:48 +0900

    1

--- Python/programmersPractice.py
+++ Python/programmersPractice.py
@@ -1,15 +1,9 @@
 
-
-
-
-
-
-
 #[PCCE 기출문제] 3번 / 나이 계산
 year = int(input())
 age_type = input()
 
-if age_type == "korea":
+if age_type == "Korea":
     answer = 2030-year+1
 
 elif age_type == "Year":
