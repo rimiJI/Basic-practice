dd9c5e31393b7c665f4c7b26ccd945086c2b3ecd
Hyerim JI <rimirimijj@gmail.com>
2024-09-04 21:48:15 +0900

    240904

--- Python/programmersPractice.py
+++ Python/programmersPractice.py
@@ -1,3 +1,37 @@
+#[PCCE 기출문제] 5번 / 산책 /C++
+'''이번은 C++로 풀었다.case에 관한 문제로 
+single quots안에 표기함
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string route) {
+    int east = 0;
+    int north = 0;
+    vector<int> answer(2);
+    for(int i=0; i<route.length(); i++){
+        switch(route[i]){
+            case 'N':
+                north++;
+                break;
+            case 'S':
+                north--;    <-이거빈칸문제였음
+                break;
+            case 'E':
+                east++;    <-이거빈칸문제였음
+                break;
+            case 'W':
+                east--;    <-이거빈칸문제였음
+                break;    <-이거빈칸문제였음
+        }
+    }
+    answer[0] = east;
+    answer[1] = north;
+    return answer;
+}
+'''
+
 
 #[PCCE 기출문제] 3번 / 나이 계산
 year = int(input())
