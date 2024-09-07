6e4321c750f05466fc3819d1e7237d0f6cde90ba
Hyerim JI <rimirimijj@gmail.com>
2024-09-07 23:37:40 +0900

    240907

--- Python/programmersPractice.py
+++ Python/programmersPractice.py
@@ -1,3 +1,40 @@
+#[PCCE 기출문제] 7번 / 가습기
+def func1(humidity, val_set):
+    if humidity < val_set:
+        return 3
+    return 1
+
+def func2(humidity):
+    if humidity >= 50:
+        return 0
+    elif humidity >= 40:
+        return 1
+    elif humidity >= 30:
+        return 2
+    elif humidity >= 20:
+        return 3
+    elif humidity >= 10:
+        return 4
+    else:
+        return 5
+
+def func3(humidity, val_set):
+    if humidity < val_set:
+        return 1
+    return 0
+
+def solution(mode_type, humidity, val_set):
+    answer = 0
+    if mode_type == "auto":
+        answer = func2(humidity)
+    elif mode_type == "target":
+        answer = func1(humidity, val_set)
+    elif mode_type == "minimum":
+        answer = func3(humidity, val_set)
+    return answer
+
+
+
 #[PCCE 기출문제] 5번 / 산책 /C++
 '''이번은 C++로 풀었다.case에 관한 문제로 
 single quots안에 표기함
